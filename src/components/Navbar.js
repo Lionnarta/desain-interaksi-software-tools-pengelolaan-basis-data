70de9460dbadefe675d0d7b5f4172dfbc1f08144
LionnartaS <lionnarta@akupintar.id>
2024-07-08 18:00:45 +0700

    navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,33 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+  const location = useLocation();
   return (
-    <div>
-      <Link to="/">Basis Data</Link>
-      <Link to="/tabel">Tabel</Link>
-      <Link to="/dokumentasi">Dokumentasi</Link>
+    <div className="w-[300px] h-screen p-10 bg-[#D4D4D4] font-poppins text-black">
+      <h1 className="text-4xl font-bold text-center mb-[50px]">DATABASE</h1>
+      <div>
+        <div
+          className={`text-2xl font-semibold ${
+            location.pathname === "/" ? "bg-[#ECECEC]" : ""
+          } px-4 py-3 rounded-md mb-[10px]`}
+        >
+          <Link to="/">Basis Data</Link>
+        </div>
+        <div
+          className={`text-2xl font-semibold ${
+            location.pathname === "/tabel" ? "bg-[#ECECEC]" : ""
+          } px-4 py-3 rounded-md mb-[10px]`}
+        >
+          <Link to="/tabel">Tabel</Link>
+        </div>
+        <div
+          className={`text-2xl font-semibold ${
+            location.pathname === "/dokumentasi" ? "bg-[#ECECEC]" : ""
+          } px-4 py-3 rounded-md mb-[10px]`}
+        >
+          <Link to="/dokumentasi">Dokumentasi</Link>
+        </div>
+      </div>
     </div>
   );
 };
--- src/routes/BasisData.js
+++ src/routes/BasisData.js
@@ -1,5 +1,13 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+
 const BasisData = () => {
-  return <div className="text-3xl">Basis Data</div>;
+  return (
+    <>
+      {/* <FontAwesomeIcon icon={faMagnifyingGlass} /> */}
+      <div className="text-3xl">Basis Data</div>;
+    </>
+  );
 };
 
 export default BasisData;
